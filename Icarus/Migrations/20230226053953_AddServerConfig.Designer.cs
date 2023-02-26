02f527978def1460b14f10532bca9544805c8eda
GuyInGrey <grey@guyingrey.com>
2023-02-25 22:41:33 -0700

    ServerSettingMigration

--- Icarus/Migrations/20230226053953_AddServerConfig.Designer.cs
+++ Icarus/Migrations/20230226053953_AddServerConfig.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Icarus;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Icarus.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230226053953_AddServerConfig")]
+    partial class AddServerConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
